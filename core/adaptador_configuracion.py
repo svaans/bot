be30a0a970c7e9d183fbef60e3368ac0c83bb964
svaans <santiagovasquez2711@gmail.com>
2025-06-14 09:04:09 +0200

    trader

--- core/adaptador_configuracion.py
+++ core/adaptador_configuracion.py
@@ -44,7 +44,7 @@ def configurar_parametros_dinamicos(symbol: str, df: pd.DataFrame, base_config: 
 
     # === Diversidad mínima adaptativa ===
     if slope < -0.002:  # mercado muy bajista
-        config["diversidad_minima"] = 3
+        config["diversidad_minima"] = 2
     elif slope > 0.002:
         config["diversidad_minima"] = 1
     else:
