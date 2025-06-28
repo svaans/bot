99487fa61dc3f1b27d94a26d27b3dec4730cb025
svaans <santiagovasquez2711@gmail.com>
2025-06-28 06:59:21 +0200

    go

--- candle_service/main.go
+++ candle_service/main.go
@@ -9,87 +9,111 @@ import (
 	"time"
 
 	pb "candle_service/proto"
-    "github.com/gorilla/websocket"
+	"github.com/gorilla/websocket"
 	"google.golang.org/grpc"
-    "google.golang.org/grpc/codes"
-    "google.golang.org/grpc/status"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var validIntervals = map[string]struct{}{
 	"1m": {}, "3m": {}, "5m": {}, "15m": {}, "30m": {},
-    "1h": {}, "2h": {}, "4h": {}, "6h": {}, "8h": {},
-    "12h": {}, "1d": {}, "3d": {}, "1w": {}, "1M": {},
+	"1h": {}, "2h": {}, "4h": {}, "6h": {}, "8h": {},
+	"12h": {}, "1d": {}, "3d": {}, "1w": {}, "1M": {},
 }
 
-type server struct{
-    pb.UnimplementedCandleServiceServer
+type server struct {
+	pb.UnimplementedCandleServiceServer
 }
 
 func normalizeSymbol(s string) string {
-    return strings.ToLower(strings.ReplaceAll(s, "/", ""))
+	return strings.ToLower(strings.ReplaceAll(s, "/", ""))
 }
 
 func (s *server) Subscribe(req *pb.CandleRequest, stream pb.CandleService_SubscribeServer) error {
-    if _, ok := validIntervals[req.Interval]; !ok {
-        return status.Errorf(codes.InvalidArgument, "invalid interval: %s", req.Interval)
-    }
-    url := fmt.Sprintf("wss://stream.binance.com:9443/ws/%s@kline_%s", normalizeSymbol(req.Symbol), req.Interval)
-    backoff := time.Second
+	if _, ok := validIntervals[req.Interval]; !ok {
+		return status.Errorf(codes.InvalidArgument, "invalid interval: %s", req.Interval)
+	}
 
-    for {
-        if err := stream.Context().Err(); err != nil {
-            return err
-        }
+	url := fmt.Sprintf("wss://stream.binance.com:9443/ws/%s@kline_%s", normalizeSymbol(req.Symbol), req.Interval)
+	backoff := time.Second
 
-        ws, _, err := websocket.DefaultDialer.Dial(url, nil)
-        if err != nil {
-            log.Printf("connect error: %v", err)
-            time.Sleep(backoff)
-            if backoff < 30*time.Second {
-                backoff *= 2
-            }
-            continue
-        }
-        log.Printf("connected %s %s", req.Symbol, req.Interval)
+	for {
+		if err := stream.Context().Err(); err != nil {
+			return err
+		}
+
+		ws, _, err := websocket.DefaultDialer.Dial(url, nil)
+		if err != nil {
+			log.Printf("connect error: %v", err)
+			time.Sleep(backoff)
+			if backoff < 30*time.Second {
+				backoff *= 2
+			}
+			continue
+		}
+		log.Printf("connected %s %s", req.Symbol, req.Interval)
 		backoff = time.Second
-        for {
+
+		for {
 			if err := stream.Context().Err(); err != nil {
-                ws.Close()
+				ws.Close()
 				return err
 			}
+
 			_, msg, err := ws.ReadMessage()
 			if err != nil {
 				log.Printf("read error: %v", err)
 				ws.Close()
 				break
 			}
+
 			var data struct {
 				K struct {
-					T int64   `json:"t"`
-					O float64 `json:"o"`
-					H float64 `json:"h"`
-					L float64 `json:"l"`
-					C float64 `json:"c"`
-					V float64 `json:"v"`
-					X bool    `json:"x"`
+					T int64       `json:"t"`
+					O json.Number `json:"o"`
+					H json.Number `json:"h"`
+					L json.Number `json:"l"`
+					C json.Number `json:"c"`
+					V json.Number `json:"v"`
+					X bool        `json:"x"`
 				} `json:"k"`
 			}
+
 			if err := json.Unmarshal(msg, &data); err != nil {
 				log.Printf("json error: %v", err)
 				continue
 			}
+
 			if !data.K.X {
 				continue
 			}
+
 			candle := &pb.Candle{
 				Symbol:    req.Symbol,
 				Timestamp: data.K.T,
-				Open:      data.K.O,
-				High:      data.K.H,
-				Low:       data.K.L,
-				Close:     data.K.C,
-				Volume:    data.K.V,
 			}
+
+			if candle.Open, err = data.K.O.Float64(); err != nil {
+				log.Printf("parse Open error: %v", err)
+				continue
+			}
+			if candle.High, err = data.K.H.Float64(); err != nil {
+				log.Printf("parse High error: %v", err)
+				continue
+			}
+			if candle.Low, err = data.K.L.Float64(); err != nil {
+				log.Printf("parse Low error: %v", err)
+				continue
+			}
+			if candle.Close, err = data.K.C.Float64(); err != nil {
+				log.Printf("parse Close error: %v", err)
+				continue
+			}
+			if candle.Volume, err = data.K.V.Float64(); err != nil {
+				log.Printf("parse Volume error: %v", err)
+				continue
+			}
+
 			if err := stream.Send(candle); err != nil {
 				ws.Close()
 				return err
@@ -99,14 +123,16 @@ func (s *server) Subscribe(req *pb.CandleRequest, stream pb.CandleService_Subscr
 }
 
 func main() {
-    lis, err := net.Listen("tcp", ":9000")
-    if err != nil {
-        log.Fatal(err)
-    }
-    grpcServer := grpc.NewServer()
-    pb.RegisterCandleServiceServer(grpcServer, &server{})
-    log.Println("candle service listening on :9000")
-    if err := grpcServer.Serve(lis); err != nil {
-        log.Fatal(err)
-    }
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCandleServiceServer(grpcServer, &server{})
+
+	log.Println("candle service listening on :9000")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
 }
