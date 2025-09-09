7cf18234709bd43c715d95e0c3bec9a194bea8d2
svaans <102782670+svaans@users.noreply.github.com>
2025-09-09 17:11:49 +0200

    Update __init__.py

--- core/strategies/__init__.py
+++ core/strategies/__init__.py
@@ -2,14 +2,14 @@ from .strategy_engine import StrategyEngine
 from .pesos import gestor_pesos, cargar_pesos_estrategias, obtener_peso_salida
 from .ajustador_pesos import ajustar_pesos_por_desempeno
 from core.evaluacion_tecnica import evaluar_estrategias
-from .evaluador_tecnico import evaluar_puntaje_tecnico, calcular_umbral_adaptativo as calc_umbral_tecnico, cargar_pesos_tecnicos, actualizar_pesos_tecnicos
+from .evaluador_tecnico import evaluar_puntaje_tecnico, cargar_pesos_tecnicos, actualizar_pesos_tecnicos
 from core.scoring import calcular_score_tecnico
 from core.estrategias import obtener_estrategias_por_tendencia, filtrar_por_direccion, ESTRATEGIAS_POR_TENDENCIA, calcular_sinergia
 from .tendencia import detectar_tendencia
 from .entry.analisis_previo import *
 __all__ = ['StrategyEngine', 'gestor_pesos', 'cargar_pesos_estrategias',
     'obtener_peso_salida', 'ajustar_pesos_por_desempeno',
-    'evaluar_estrategias', 'evaluar_puntaje_tecnico', 'calc_umbral_tecnico',
+    'evaluar_estrategias', 'evaluar_puntaje_tecnico',
     'cargar_pesos_tecnicos', 'actualizar_pesos_tecnicos',
     'calcular_score_tecnico', 'obtener_estrategias_por_tendencia',
     'filtrar_por_direccion', 'ESTRATEGIAS_POR_TENDENCIA',
