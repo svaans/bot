13859fb8b8200c5969eee5852869441e136bc191
svaans <102782670+svaans@users.noreply.github.com>
2025-09-09 16:26:02 +0200

    Update __init__.py

--- core/strategies/__init__.py
+++ core/strategies/__init__.py
@@ -3,7 +3,7 @@ from .pesos import gestor_pesos, cargar_pesos_estrategias, obtener_peso_salida
 from .ajustador_pesos import ajustar_pesos_por_desempeno
 from core.evaluacion_tecnica import evaluar_estrategias
 from .evaluador_tecnico import evaluar_puntaje_tecnico, calcular_umbral_adaptativo as calc_umbral_tecnico, cargar_pesos_tecnicos, actualizar_pesos_tecnicos
-from core.score_tecnico import calcular_score_tecnico
+from core.scoring import calcular_score_tecnico
 from core.estrategias import obtener_estrategias_por_tendencia, filtrar_por_direccion, ESTRATEGIAS_POR_TENDENCIA, calcular_sinergia
 from .tendencia import detectar_tendencia
 from .entry.analisis_previo import *
