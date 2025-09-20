3463627c86e64a2beaf75c9a2670e6e21f500778
svaans <santiagovasquez2711@gmail.com>
2025-09-21 00:28:51 +0200

    refactor

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+"""Test suite for the trading bot.
+
+This package contains a collection of unit tests for core modules that
+should function independently of external services.  The tests are
+written using Python's builtin ``unittest`` framework so they can run
+without additional dependencies.
+"""
+
+__all__ = []
