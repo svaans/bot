ad0f132666f48224b4a45b375d196cef09625073
svaans <102782670+svaans@users.noreply.github.com>
2025-10-09 12:40:39 +0200

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,9 @@
 from importlib import import_module
+
+from core.utils.asyncio_compat import ensure_taskgroup
+
+ensure_taskgroup()
+
 __all__ = ['data', 'risk', 'orders', 'strategies', 'utils', 'streams']
 
 
